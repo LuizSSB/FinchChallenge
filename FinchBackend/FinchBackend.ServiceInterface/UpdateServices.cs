178280e3a08d0622fb829c92c2a5bc39b8d8e7a9
luizssb <luizssb.biz@gmail.com>
2018-12-13 08:01:18 -0200

    Serviços de edição de dados.

---
+++ FinchBackend/FinchBackend.ServiceInterface/UpdateServices.cs
@@ -0,0 +1,52 @@
+﻿using FinchBackend.ServiceModel;
+using ServiceStack;
+using ServiceStack.Data;
+using ServiceStack.OrmLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinchBackend.ServiceInterface
+{
+    // [Authenticated]
+    public class UpdateServices : Service
+    {
+        public IDbConnectionFactory DbConnection { get; set; }
+
+        public void Put(UpdateProtest request)
+        {
+            using (var db = DbConnection.Open())
+            {
+                if (request.UpdatesReferences && request.Protest.Payment != null)
+                {
+                    if (request.Protest.Payment.Debtor != null)
+                    {
+                        db.InsertOrReplace(request.Protest.Payment.Debtor);
+                    }
+
+                    db.InsertOrReplace(request.Protest.Payment);
+                }
+
+                db.Update(request.Protest);
+            }
+        }
+
+        public void Put(UpdatePayment request)
+        {
+            using (var db = DbConnection.Open())
+            {
+                db.Update(request.Payment);
+            }
+        }
+
+        public void Put(UpdateDebtor request)
+        {
+            using (var db = DbConnection.Open())
+            {
+                db.Update(request.Debtor);
+            }
+        }
+    }
+}
---
+++ FinchBackend/FinchBackend.ServiceModel/UpdateProtest.cs
@@ -0,0 +1,32 @@
+﻿using ServiceStack;
+using System.Runtime.Serialization;
+
+namespace FinchBackend.ServiceModel
+{
+    [DataContract]
+    [Route("/protests/update")]
+    public class UpdateProtest : BaseRequest
+    {
+        [DataMember(Name = "protest")]
+        public Types.PaymentProtest Protest { get; set; }
+
+        [DataMember(Name = "updatesReferences")]
+        public bool UpdatesReferences { get; set; }
+    }
+
+    [DataContract]
+    [Route("/payment")]
+    public class UpdatePayment : BaseRequest
+    {
+        [DataMember(Name = "payment")]
+        public Types.Payment Payment { get; set; }
+    }
+
+    [DataContract]
+    [Route("/debtor")]
+    public class UpdateDebtor : BaseRequest
+    {
+        [DataMember(Name = "debtor")]
+        public Types.Debtor Debtor { get; set; }
+    }
+}
